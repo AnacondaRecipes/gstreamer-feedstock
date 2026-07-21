0ea13a13f563bc10dba169dc9db0c9a5762f807f
vkhomits <khomitsvalia@gmail.com>
2026-07-21 14:16:07 +0300

    Link intl.lib

--- recipe/install_good_plugins.bat
+++ recipe/install_good_plugins.bat
@@ -7,9 +7,21 @@ set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%B
 :: get mixed path (forward slash) form of prefix so host prefix replacement works
 set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
 
+:: Same gettext/intl linkage issue as gstreamer core: GLib is built with
+:: gettext support even though this module's own nls option is disabled,
+:: so any TU using <glib/gi18n.h> (here: tools/gst-play.c) still needs
+:: intl.lib at link time.
+set "LDFLAGS=%LDFLAGS% %LIBRARY_LIB%\intl.lib"
+
 cd plugins_good
 
-%BUILD_PREFIX%\Scripts\meson.exe setup builddir --wrap-mode=nofallback --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dexamples=disabled -Dtests=disabled -Dpackage-origin=https://github.com/AnacondaRecipes/gstreamer-feedstock
+%BUILD_PREFIX%\Scripts\meson.exe setup builddir ^
+--wrap-mode=nofallback ^
+--buildtype=release ^
+--prefix=%LIBRARY_PREFIX_M% --backend=ninja ^
+-Dexamples=disabled ^
+-Dtests=disabled ^
+-Dpackage-origin=https://github.com/AnacondaRecipes/gstreamer-feedstock
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
